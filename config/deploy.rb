a5af08237941c2f2878ae85da62eb132b4108016
Mark <mplennon@gmail.com>
2015-03-19 16:19:23 +1000

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock '3.2.1'
 
 set :application, 'translator'
 set :repo_url, "ssh://gitrepo@git.dhamma.org.au:8022/home/gitrepo/repositories/#{fetch(:application)}"
-set :rvm1_ruby_version, "rbx-2.2.10"
-
+#set :rvm1_ruby_version, "rbx-2.2.10"
+set :rvm1_ruby_version, "ruby-2.1.3"
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
