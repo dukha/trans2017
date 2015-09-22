f43025dfd9c4f3017bd1c09ce1a74b7c58ad57dd
Mark <mplennon@gmail.com>
2015-09-22 14:24:46 +1000

    fixes in upload also tooltips and deep copy job2

---
+++ app/jobs/finish_version_deep_copy_job.rb
@@ -0,0 +1,17 @@
+class FinishVersionDeepCopyJob< BaseJob
+  queue_as :default
+
+  def perform(old_version_id, new_version_id, user_id, copy_translation_languages, copy_translations)
+    begin
+    CalmappVersion.finish_deep_copy(old_version_id, new_version_id, user_id, copy_translation_languages, copy_translations )
+    rescue Exception => exception
+      ExceptionNotifier.notify_exception(exception,
+      {:data=> {:class=> CalmappVersion, 
+        :old_version_id => old_version_id, :new_version_id=> new_version_id, :method=>"finish_deep_copy"}})
+        
+        info "Exception in deep_destroy() " + exception.message 
+        exception_raised
+        raise
+    end  
+  end  
+end
